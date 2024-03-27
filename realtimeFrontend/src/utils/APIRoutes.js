a220697aa9789c03595901d1782f6505ad7e8aff
aryan1625 <117471320+aryan1625@users.noreply.github.com>
2024-03-27 13:32:22 +0530

    Update APIRoutes.js

--- realtimeFrontend/src/utils/APIRoutes.js
+++ realtimeFrontend/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://chatty-backend-three.vercel.app:3000";
+export const host = "https://chatty-backend.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
