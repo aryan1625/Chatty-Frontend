ad97460ab3884b12dd528d643e9752aea4c10e84
aryan1625 <117471320+aryan1625@users.noreply.github.com>
2024-03-27 16:21:45 +0530

    Delete realtimeFrontend directory

--- realtimeFrontend/src/utils/APIRoutes.js
+++
@@ -1,7 +0,0 @@
-export const host = "https://chatty-backend.onrender.com";
-export const registerRoute = `${host}/api/auth/register`;
-export const loginRoute = `${host}/api/auth/login`;
-export const setAvatarRoute = `${host}/api/auth/setAvatar`;
-export const allUsersRoute = `${host}/api/auth/allUsers`;
-export const addMessageRoute = `${host}/api/messages/addmsg`;
-export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
